e599e0b16355c528ebfdb4be59e6ff3949c9f8be
Alvin Ng <alvin.cl.ng@gmail.com>
2022-10-22 11:47:31 -0700

    Completed function for sum of Array

--- M1W1-Intro-to-dev/problem.js
+++ M1W1-Intro-to-dev/problem.js
@@ -1,7 +1,25 @@
-console.log(process.argv);
+// console.log(process.argv);
 
 const commandLineArguments = process.argv;
 
 const sliced = commandLineArguments.slice(2);
 
-console.log(sliced)
+// console.log(sliced);
+
+function sumOfArr(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    const num = Number(arr[i]);
+    if(typeof num === 'number' && num >= 0 && num % 1 === 0) {      
+      sum += num;
+    }
+  }
+  return sum
+}
+
+const four = 4;
+console.log(four + 6)
+
+sumOfArr(sliced);
+
+console.log(sumOfArr(sliced) + 10)
